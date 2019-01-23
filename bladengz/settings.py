c36c843bd3f202ad3d2a719122b1c066f29f8d25
Blessed Sibanda <blessedsibanda434@gmail.com>
2019-01-23 14:40:02 +0200

    Update settings.py

--- bladengz/settings.py
+++ bladengz/settings.py
@@ -138,9 +138,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bladengz', 'static'),
 ]
 
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'bladengz', 'media')
 MEDIA_URL = '/media/'
